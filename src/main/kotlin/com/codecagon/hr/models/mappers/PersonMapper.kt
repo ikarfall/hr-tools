9ddaa4f2f47682738935ed0422920d91fd84dcb5
yaroslav.prokipchyn <yaroslav.prokipchyn@gmail.com>
2020-11-25 14:53:59 +0200

    add complex mapping for person

--- src/main/kotlin/com/codecagon/hr/models/mappers/PersonMapper.kt
+++ src/main/kotlin/com/codecagon/hr/models/mappers/PersonMapper.kt
@@ -4,14 +4,21 @@ import com.codecagon.hr.entities.ProjectAssignment
 import com.codecagon.hr.entities.SalaryAssignment
 import com.codecagon.hr.models.Person
 import org.mapstruct.Mapper
+import org.mapstruct.Mapping
+import org.mapstruct.Mappings
+
 import com.codecagon.hr.entities.Person as PersonEntity
 
 @Mapper(componentModel = "spring")
 interface PersonMapper {
-    //TODO most likely does not work explicit implementation is needed
+    @Mappings(
+            Mapping(source = "projectAssignments", target = "projectAssignments"),
+            Mapping(source = "salaryAssignments", target = "salaryAssignments")
+    )
     fun toModel(person: PersonEntity,
                 projectAssignments: List<ProjectAssignment>,
                 salaryAssignments: List<SalaryAssignment>): Person
+
 
     fun fromModel(person: Person): PersonEntity
 
